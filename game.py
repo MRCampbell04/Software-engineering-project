14967e815dfd253928b08aad0279118beee4350f
dina-elhami <dina20044@gmail.com>
2025-12-02 16:41:38 +0200

    Update game.py

--- game.py
+++ game.py
@@ -3,8 +3,6 @@ import sys
 import random
 import os
 
-sys.path.append(r"C:\Users\dinae\Downloads\Tetris")    # يحتوي على board.py و home.py
-
 # ------------------- Imports ------------------
 from shapes import Shape, create_shape
 from board import Board, GRID_X, GRID_Y, CELL_SIZE, BLACK, WHITE, RED, font_path
